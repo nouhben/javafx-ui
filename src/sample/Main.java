77d112af8faa61e27cffc7ba4194ada80e25c532
nouh ben kadi <benkadi.nouh@icloud.com>
2020-03-16 23:14:40 +0100

    final commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        FXMLLoader loader = new FXMLLoader();
+        try{
+            loader.setLocation(this.getClass().getResource("./sample.fxml"));
+            BorderPane root = loader.load();
+            Scene interface1 = new Scene(root);
+            primaryStage.setScene(interface1);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
